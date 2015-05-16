a616c06a93854a8916db824e75db5ba48eee44ac
MitsushigeKomiya <s.wyvern.acespring@gmail.com>
2015-05-16 12:48:13 +0000

    moidfy it

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 // create socket io's server
-var io = require('socket.io')(8080);
+var server = require('http').Server();
+var socketIO = require('socket.io');
+var io = socketIO.listen(server);
 
 io.on('connection', function(socket) {
-  // send connect notification for client
-  io.emit("sendMessageToClient", {value: "Entered one person."});
+  console.log("connected");
 
-  // setting recieve event by client
-  socket.on("sendMessageToServer", function(data) {
-    io.emit("sendMessageToClient", {value: data.value});
+  // setting chat command
+  socket.on('hello', function(data) {
+    console.log("hello : " + data.value);
+    io.sockets.emit('hello', { value: data.value });
   });
 
   // setting disconnect event
   socket.on("disconnect", function() {
-    io.emit("sendMessageToClient", {value: "Withdraw one person."});
+    console.log("disconnected");
   });
+
+  io.sockets.emit('hello', { value: "welcome" });
 });
+server.listen(3000);
