310525b70d56999fd987232c80ce6d6667baa45f
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2015-05-19 21:41:51 +0900

    add publish connected player id

--- server/app.js
+++ server/app.js
@@ -3,8 +3,16 @@ var server = require('http').Server();
 var socketIO = require('socket.io');
 var io = socketIO.listen(server);
 
+var player_list = [];
+
 io.on('connection', function(socket) {
   console.log("connected");
+
+  var player_id = Math.random() * 2147483647;
+  player_list.push(player_id);
+
+  console.log("player_list:");
+  console.log(player_list);
 
   // setting chat command
   socket.on('hello', function(data) {
