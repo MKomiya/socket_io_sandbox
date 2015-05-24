a9c49ea0686549676bc5775fabd7b6373522b882
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2015-05-24 12:51:14 +0900

    add generate player id and disconnect subscribe

--- socket_server/routes/model/skio.js
+++ socket_server/routes/model/skio.js
@@ -2,6 +2,8 @@ var app  = require('../../app');
 var http = require('http').Server(app);
 var io   = require('socket.io').listen(http);
 
+var player_list = [];
+
 function skio() {
   http.listen(app.get('port'), function() {
     console.log('Start listening...');
@@ -9,9 +11,20 @@ function skio() {
   });
 
   io.on('connection', function(socket) {
+    var player_id = Math.floor(Math.random() * 2147483647);
+    player_list.push(player_id);
+
+    console.log("player_list:");
+    console.log(player_list);
+
     socket.on('hello', function(data) {
       console.log('message: ' + data.value);
       socket.emit('hello', { value : data.value});
+    });
+
+    socket.on("disconnect", function() {
+      player_list.pop();
+      console.log("disconnected");
     });
   });
 }
