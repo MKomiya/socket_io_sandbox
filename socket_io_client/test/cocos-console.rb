c993e95c4878167fa50b585799586a6f62a26e80
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2015-05-29 21:48:23 +0900

    tmp

--- socket_io_client/test/cocos-console.rb
+++ socket_io_client/test/cocos-console.rb
@@ -6,10 +6,10 @@ if ARGV[0] == nil then
   return;
 end
 
-# 127.0.0.1(localhost)の20000番へ接続
 sock = TCPSocket.open("localhost", 6010)
 
-sock.write("hello #{ARGV[0]}\n")
+command = "hello #{ARGV[0]}\n"
 
-# 送信が終わったらソケットを閉じる
+sock.write(command)
+
 sock.close
