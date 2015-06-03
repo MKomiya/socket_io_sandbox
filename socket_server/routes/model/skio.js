10897d3c27fb6ef9ee6a14b5b7bdf327d7ffefe4
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2015-06-03 21:14:34 +0900

    added join end send message for the same members

--- socket_server/routes/model/skio.js
+++ socket_server/routes/model/skio.js
@@ -22,6 +22,18 @@ function skio() {
       socket.emit('hello', { value : data.value});
     });
 
+    socket.on('init', function(data) {
+      socket.setRoomInfo = data.room;
+      socket.join(data.room);
+      socket.join(socket.id);
+
+      console.log(data.room + " join");
+    });
+
+    socket.on('msg_post', function(data) {
+      socket.to(socket.setRoomInfo).emit("hello", data.msg);
+    })
+
     socket.on('move', function(data) {
       console.log('moved: (' + data.x + ', ' + data.y + ')');
       socket.emit('move', { x : data.x, y : data.y });
