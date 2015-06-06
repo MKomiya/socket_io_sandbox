fe3d7d6759ba6d38949ce8936d650998a770e77e
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2015-06-06 21:56:46 +0900

    Remove player list that instead of socket id

--- socket_server/routes/model/skio.js
+++ socket_server/routes/model/skio.js
@@ -2,8 +2,6 @@ var app  = require('../../app');
 var http = require('http').Server(app);
 var io   = require('socket.io').listen(http);
 
-var player_list = [];
-
 function skio() {
   http.listen(app.get('port'), function() {
     console.log('Start listening...');
@@ -11,11 +9,6 @@ function skio() {
   });
 
   io.on('connection', function(socket) {
-    var player_id = Math.floor(Math.random() * 2147483647);
-    player_list.push(player_id);
-
-    console.log("player_list:");
-    console.log(player_list);
 
     socket.on('hello', function(data) {
       console.log('message: ' + data.value);
