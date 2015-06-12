af7ef680d5146ed06a3b03820e1fbf1d0d5fc61c
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2015-06-13 00:42:19 +0900

    add matching system base in server side

--- socket_server/routes/model/skio.js
+++ socket_server/routes/model/skio.js
@@ -24,6 +24,14 @@ function skio() {
 
       console.log(data.room + " join");
       console.log("user_id: " + socket.id);
+
+      socket.emit('joined_room', { id : socket.id });
+    });
+
+    socket.on('battle_proposal', function(data) {
+      var battle_id = Math.floor(Math.random() * 2147483647);
+      socket.to(data.to).emit('battle_proposal', {from: socket.id, to: data.to, battle_id: battle_id});
+      start_battle(battle_id);
     });
 
     socket.on('get_rooms', function(data) {
@@ -42,7 +50,22 @@ function skio() {
     socket.on("disconnect", function() {
       console.log("disconnected");
     });
+
+    // 接続の確立をクライアントに通知
+    socket.emit('connected', { id : socket.id });
+
+    // バトル開始
+    start_battle(battle_id);
   });
+}
+
+function start_battle(battle_id) {
+  var battle = io.of('/battle/', + battle_id).on('connection', function(socket) {
+    socket.on('game_start', function() {
+      console.log('started game');
+      socket.emit('game_start', {});
+    })
+  })
 }
 
 module.exports = skio;
