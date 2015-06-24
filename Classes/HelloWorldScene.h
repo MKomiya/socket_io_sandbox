5dc601d7c7bc33b0e94b273a8ea3457f2e55fb47
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2015-06-23 22:33:50 -0700

    Update HelloWorldScene.h

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -33,10 +33,7 @@ public:
     void addTalkPlayer(const std::string& str);
     void addTalkOther(const std::string& str);
 
-    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
-
-    // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
 };
 
